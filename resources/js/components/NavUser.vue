9822144d19b65baa9e3beec3d549716b57780a3b
jovianka <jovianpratama123@gmail.com>
2025-05-21 15:05:46 +0800

    Remove redundant prop passing

--- resources/js/components/NavUser.vue
+++ resources/js/components/NavUser.vue
@@ -2,13 +2,9 @@
 import UserInfo from '@/components/UserInfo.vue';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 import { SidebarMenu, SidebarMenuButton, SidebarMenuItem, useSidebar } from '@/components/ui/sidebar';
-import { type SharedData, type User } from '@/types';
-import { usePage } from '@inertiajs/vue3';
 import { ChevronsUpDown } from 'lucide-vue-next';
 import UserMenuContent from './UserMenuContent.vue';
 
-const page = usePage<SharedData>();
-const user = page.props.auth.user as User;
 const { isMobile, state } = useSidebar();
 </script>
 
@@ -18,7 +14,7 @@ const { isMobile, state } = useSidebar();
             <DropdownMenu>
                 <DropdownMenuTrigger as-child>
                     <SidebarMenuButton size="lg" class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground">
-                        <UserInfo :user="user" />
+                        <UserInfo />
                         <ChevronsUpDown class="ml-auto size-4" />
                     </SidebarMenuButton>
                 </DropdownMenuTrigger>
@@ -28,7 +24,7 @@ const { isMobile, state } = useSidebar();
                     align="end"
                     :side-offset="4"
                 >
-                    <UserMenuContent :user="user" />
+                    <UserMenuContent/>
                 </DropdownMenuContent>
             </DropdownMenu>
         </SidebarMenuItem>
