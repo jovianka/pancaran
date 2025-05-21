34c4d56352d7f3434cea9e212bbaea6f70737dee
jovianka <jovianpratama123@gmail.com>
2025-05-21 10:59:10 +0800

    Profile settings and Avatar

--- app/Http/Requests/Settings/ProfileUpdateRequest.php
+++ app/Http/Requests/Settings/ProfileUpdateRequest.php
@@ -15,16 +15,51 @@ class ProfileUpdateRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => [
-                'required',
-                'string',
-                'lowercase',
-                'email',
-                'max:255',
+        $nimRules = '';
+        if ($this->user()->type == 'student') {
+            $nimRules = 'string|max:255';
+        } else {
+            $nimRules = 'prohibited';
+        }
+
+        $emailRules = [];
+        if ($this->user()->type == 'student') {
+            $emailRules = [
+                'string', 'lowercase', 'email', 'ends_with:student.unud.ac.id', 'max:255',
                 Rule::unique(User::class)->ignore($this->user()->id),
-            ],
+            ];
+        } else {
+            $emailRules = [
+                'string', 'lowercase', 'email', 'ends_with:unud.ac.id', 'max:255',
+                Rule::unique(User::class)->ignore($this->user()->id),
+            ];
+        }
+
+        return [
+            'avatar' => 'nullable|file|image',
+            'nim' => $nimRules,
+            'name' => 'string|max:255',
+            'email' => $emailRules,
+            'faculty_id' => 'integer|exists:faculty,id',
+            'major_id' => [
+                'integer',
+                Rule::exists('major', 'id')->where('faculty_id', $this->faculty_id),
+                'required_with:faculty_id'
+            ]
         ];
     }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'faculty_id.exists' => 'faculty doesn\'t exist',
+            'major_id.exists' => 'This major doesn\'t belong to the selected faculty'
+        ];
+    }
+
 }
--- resources/js/components/UserInfo.vue
+++ resources/js/components/UserInfo.vue
@@ -1,34 +1,39 @@
 <script setup lang="ts">
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Avatar, AvatarImage } from '@/components/ui/avatar';
 import { useInitials } from '@/composables/useInitials';
-import type { User } from '@/types';
+import { cn } from '@/lib/utils';
+import { SharedData, type User } from '@/types';
+import { usePage } from '@inertiajs/vue3';
 import { computed } from 'vue';
 
 interface Props {
-    user: User;
     showEmail?: boolean;
 }
 
 const props = withDefaults(defineProps<Props>(), {
-    showEmail: false,
+    showEmail: true,
 });
+
+const page = usePage<SharedData>();
+const user = computed(() => page.props.auth.user as User);
 
 const { getInitials } = useInitials();
 
 // Compute whether we should show the avatar image
-const showAvatar = computed(() => props.user.avatar && props.user.avatar !== '');
+const showAvatar = computed(() => user.value.avatar && user.value.avatar !== '');
 </script>
 
 <template>
     <Avatar class="h-8 w-8 overflow-hidden rounded-lg">
-        <AvatarImage v-if="showAvatar && user.avatar" :src="user.avatar" :alt="user.name" />
-        <AvatarFallback class="rounded-lg text-black dark:text-white">
+        <AvatarImage v-if="showAvatar && user.avatar" :src="`/storage/${user.avatar}`" :alt="user.name" />
+        <!-- Custom avatar fallback -->
+        <div v-show="!user.avatar" :class="cn('bg-muted flex size-full items-center justify-center rounded-full')">
             {{ getInitials(user.name) }}
-        </AvatarFallback>
+        </div>
     </Avatar>
 
     <div class="grid flex-1 text-left text-sm leading-tight">
         <span class="truncate font-medium">{{ user.name }}</span>
-        <span v-if="showEmail" class="truncate text-xs text-muted-foreground">{{ user.email }}</span>
+        <span v-if="props.showEmail" class="text-muted-foreground truncate text-xs">{{ user.email }}</span>
     </div>
 </template>
