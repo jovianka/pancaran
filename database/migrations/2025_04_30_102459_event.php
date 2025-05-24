004021bf520dcfd45985d61191795176c6951116
Red Coat <bayubujanks@gmail.com>
2025-05-24 13:27:23 +0800

    add timestamps to event

--- database/migrations/2025_04_30_102459_event.php
+++ database/migrations/2025_04_30_102459_event.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->date('start_date');
             $table->date('end_date');
             $table->string('job_description'); // file
+            $table->timestamps();
             $table->foreignId('parent_id')->nullable()->constrained('event')->nullOnDelete();//parent event
         });
     }
