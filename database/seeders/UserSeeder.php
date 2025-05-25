174b5f071cc2224b7943c9cae1167b5507be7bd7
Red Coat <bayubujanks@gmail.com>
2025-05-25 20:20:48 +0800

    fix user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,13 +1,10 @@
 <?php
 
 namespace Database\Seeders;
-
+use Illuminate\Database\Seeder;
+use App\Models\User;
 use App\Models\Faculty;
 use App\Models\Major;
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -16,26 +13,26 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
-            'nim' => null,
-            'name' => 'Contoh UKM Universitas',
-            'email' => 'ukm@unud.ac.id',
-            'password' => Hash::make('123'),
-            'avatar' => null,
-            'faculty_id' => Faculty::where('name', '=', 'Any')->value('id'),
-            'major_id' => Major::where('name', '=', 'Any')->value('id'),
-            'type' => 'organization',
+        $this->call(
+            FacultySeeder::class,  // Seeding untuk tabel faculty dan major
+        );
+
+
+        User::factory()->count(30)->create([
+            'type' => 'student',
+            'nim' => fn () => fake()->unique()->numerify('2308561###'),
+            'email' => fn () => fake()->unique()->numerify('2308561###'). '@student.unud.ac.id',
+            'faculty_id' => fn()=>Faculty::inRandomOrder()->first()->id,
+            'major_id' => fn(array $attributes) => Major::where('faculty_id',$attributes['faculty_id'])->inRandomOrder()->first()->id,
         ]);
 
-        User::factory()->create([
-            'nim' => '123456789',
-            'name' => 'Contoh Mahasiswa',
-            'email' => 'mahasiswa@student.unud.ac.id',
-            'password' => Hash::make('123'),
-            'avatar' => null,
-            'faculty_id' => Faculty::where('name', '=', 'Matematika dan Ilmu Pengetahuan Alam')->value('id'),
-            'major_id' => Major::where('name', '=', 'Matematika')->value('id'),
-            'type' => 'student',
+        User::factory()->count(5)->create([
+            'email' => fn () => fake()->unique()->numerify('2308561###'). '@unud.ac.id',
+            'type' => 'organization',
+            'nim' => null,
+            'faculty_id' => fn()=>Faculty::inRandomOrder()->first()->id,
+            'major_id' => fn(array $attributes) => Major::where('faculty_id',$attributes['faculty_id'])->inRandomOrder()->first()->id,
         ]);
+
     }
 }
