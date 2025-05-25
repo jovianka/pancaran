ce9da8d224203851b90a2b4f1226c73335781e6d
Red Coat <bayubujanks@gmail.com>
2025-05-25 21:25:27 +0800

    Add factory to Event model

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -12,6 +13,8 @@ class Event extends Model
 {
     protected $guarded = ['id'];
     protected $table = 'event';
+
+    use HasFactory;
 
     public function users(): BelongsToMany
     {
