911134094e8baf4eec4c738eb4ba2b931bbd39ef
jovianka <jovianpratama123@gmail.com>
2025-07-10 02:46:06 +0800

    Update migration

--- database/migrations/999999_04_30_103542_update_relationships.php
+++ database/migrations/999999_04_30_103542_update_relationships.php
@@ -17,62 +17,62 @@ return new class extends Migration
         });
 
         Schema::table('major', function (Blueprint $table) {
-            $table->foreignId('faculty_id')->references('id')->on('faculty');
+            $table->foreignId('faculty_id')->references('id')->on('faculty')->cascadeOnDelete();
         });
 
         Schema::table('event_registration_response', function (Blueprint $table) {
-            $table->foreignId('user_id')->references('id')->on('users');
-            $table->foreignId('event_registration_id')->references('id')->on('event_registration');
+            $table->foreignId('user_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->foreignId('event_registration_id')->references('id')->on('event_registration')->cascadeOnDelete();
         });
 
         Schema::table('event_registration_question', function (Blueprint $table) {
-            $table->foreignId('event_registration_id')->references('id')->on('event_registration');
+            $table->foreignId('event_registration_id')->references('id')->on('event_registration')->cascadeOnDelete();
         });
 
         Schema::table('event_registration', function (Blueprint $table) {
-            $table->foreignId('event_id')->references('id')->on('event');
+            $table->foreignId('event_id')->references('id')->on('event')->cascadeOnDelete();
         });
 
         Schema::table('event_registration_role', function (Blueprint $table) {
-            $table->foreignId('event_registration_id')->references('id')->on('event_registration');
-            $table->foreignId('event_role_id')->references('id')->on('event_role');
+            $table->foreignId('event_registration_id')->references('id')->on('event_registration')->cascadeOnDelete();
+            $table->foreignId('event_role_id')->references('id')->on('event_role')->cascadeOnDelete();
         });
 
         Schema::table('event_role_permission', function (Blueprint $table) {
-            $table->foreignId('event_permission_id')->references('id')->on('event_permission');
-            $table->foreignId('event_role_id')->references('id')->on('event_role');
+            $table->foreignId('event_permission_id')->references('id')->on('event_permission')->cascadeOnDelete();
+            $table->foreignId('event_role_id')->references('id')->on('event_role')->cascadeOnDelete();
         });
 
         Schema::table('event_role', function (Blueprint $table) {
-            $table->foreignId('event_id')->references('id')->on('event');
+            $table->foreignId('event_id')->references('id')->on('event')->cascadeOnDelete();
             $table->foreignId('detail_skp_id')->nullable()->references('id')->on('detail_skp')->onDelete('set null');
         });
 
         Schema::table('certificate', function (Blueprint $table) {
-            $table->foreignId('user_id')->references('id')->on('users');
+            $table->foreignId('user_id')->references('id')->on('users')->cascadeOnDelete();
             $table->foreignId('event_id')->references('id')->on('event');
             $table->foreignId('event_role_id')->references('id')->on('event_role');
         });
 
         Schema::table('surat_tugas', function (Blueprint $table) {
-            $table->foreignId('event_id')->references('id')->on('event');
+            $table->foreignId('event_id')->references('id')->on('event')->cascadeOnDelete();
         });
 
         Schema::table('invitation', function (Blueprint $table) {
-            $table->foreignId('event_id')->references('id')->on('event');
-            $table->foreignId('event_role_id')->references('id')->on('event_role');
-            $table->foreignId('recipient_id')->references('id')->on('users');
+            $table->foreignId('event_id')->references('id')->on('event')->cascadeOnDelete();
+            $table->foreignId('event_role_id')->references('id')->on('event_role')->cascadeOnDelete();
+            $table->foreignId('recipient_id')->references('id')->on('users')->cascadeOnDelete();
         });
 
         Schema::table('event_user', function (Blueprint $table) {
-            $table->foreignId('user_id')->references('id')->on('users');
-            $table->foreignId('event_id')->references('id')->on('event');
-            $table->foreignId('event_role_id')->references('id')->on('event_role');
+            $table->foreignId('user_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->foreignId('event_id')->references('id')->on('event')->cascadeOnDelete();
+            $table->foreignId('event_role_id')->references('id')->on('event_role')->cascadeOnDelete();
         });
 
         Schema::table('event_tag', function (Blueprint $table) {
-            $table->foreignId('tag_id')->references('id')->on('tag');
-            $table->foreignId('event_id')->references('id')->on('event');
+            $table->foreignId('tag_id')->references('id')->on('tag')->cascadeOnDelete();
+            $table->foreignId('event_id')->references('id')->on('event')->cascadeOnDelete();
         });
 
         Schema::table('event', function (Blueprint $table) {
@@ -82,7 +82,7 @@ return new class extends Migration
         });
 
         Schema::table('contact_person', function (Blueprint $table) {
-            $table->foreignId('event_id')->references('id')->on('event');
+            $table->foreignId('event_id')->references('id')->on('event')->cascadeOnDelete();
         });
 
     }
