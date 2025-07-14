0f1f9578f7c060208f62b4fa5641eba1d39f3564
Red Coat <bayubujanks@gmail.com>
2025-07-15 05:24:41 +0800

    shdcn components

---
+++ resources/js/pages/testing.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Button from '@/components/ui/button/Button.vue';
+import { router } from '@inertiajs/vue3';
+
+const goToPathWithPost = () => {
+  router.get('/edit-registration', {
+    registration_id: 23
+  })
+}
+</script>
+<template>
+    <Button @click=goToPathWithPost>Edit Form</Button>
+</template>
